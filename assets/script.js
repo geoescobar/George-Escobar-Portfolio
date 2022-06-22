71a878b6b674eae98e14996c29e09b0fbbe50aac
George Escobar <geo.escobar214@gmail.com>
2022-06-22 16:45:48 -0500

    content fade effects added

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
+// ======================= fetch Github API for archives folder =======================
 const divContainer = document.getElementById("apiNews");
 
-var requestUrl = "https://api.github.com/users/geoescobar/repos";
+const requestUrl = "https://api.github.com/users/geoescobar/repos";
 
 fetch(requestUrl)
   .then(function (response) {
@@ -21,4 +22,63 @@ fetch(requestUrl)
     }
   });
 
+// ======================= linking the icons =======================
 feather.replace();
+
+// ======================= scroll fade effects =======================
+const header = document.querySelector(".navbar");
+const sectionOne = document.querySelector(".home");
+
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px",
+};
+
+const sectionOneObserver = new IntersectionObserver(function (
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px",
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
+
+sliders.forEach((slider) => {
+  appearOnScroll.observe(slider);
+});
+
+// ======================= scroll fade effects =======================
+
